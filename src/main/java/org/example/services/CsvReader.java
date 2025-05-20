b35ff1c936b46cb37422d02eb47f4ffc26954376
M-Menger <matheus-menger@hotmail.com>
2025-05-20 12:09:08 -0300

    Primeiros passos

--- src/main/java/org/example/services/CsvReader.java
+++ src/main/java/org/example/services/CsvReader.java
@@ -1,5 +1,25 @@
 package org.example.services;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 public class CsvReader {
 
+    public static void readingCsv(String csvFile) {
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream(csvFile), "ISO-8859-1"));
+        ) {
+            String line;
+            boolean isFirstLine = true;
+
+            while((line = br.readLine()) != null) {
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                String[] headers = line.get(0);
+            }
+        }
+    }
 }
